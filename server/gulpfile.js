f4fe438d6c3946bf23038956ecbba5b02adcf27e
RambleRainbow <three_rainbow@msn.com>
2018-10-11 16:17:09 +0800

    修改，指向DIST的DOCKER目录

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -4,29 +4,29 @@ var gulp = require('gulp'),
   tsc = require('gulp-tsc');
 
 gulp.task("tsc", () => {
-  del("../docker/server/app/src");
+  del("./dist/app/src");
   gulp.src("src/**/*.ts")
   .pipe(tsc({
     module: "COMMONJS",
     target: "ES2015"
   }))
-  .pipe(gulp.dest("../docker/server/app/src"));
+  .pipe(gulp.dest("./dist/app/src"));
 });
 
 gulp.task("npm", () => {
   gulp.src("package*.json")
-  .pipe(gulp.dest("../docker/server/app"))
+  .pipe(gulp.dest("./dist/app"))
   .pipe(shell(["npm install --production"], {
-    cwd: "../docker/server/app"
+    cwd: "./dist/app"
   }));
 });
 
 gulp.task("copy", () => {
   gulp.src("src/config/*")
-  .pipe(gulp.dest("../docker/server/app/config"));
+  .pipe(gulp.dest("./dist/app/config"));
 
   gulp.src("src/views/*")
-  .pipe(gulp.dest("../docker/server/app/src/views"));
+  .pipe(gulp.dest("./dist/app/src/views"));
 })
 
 gulp.task("contribute", ["tsc","npm", "copy"], () => {
